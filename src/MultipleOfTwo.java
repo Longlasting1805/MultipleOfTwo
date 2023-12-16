15ceca4247cf8bf332c5c6531127b3f97d41a6df
Akande Al-ameen <longlasting1805@gmail.com>
2023-12-16 18:54:05 +0100

    first commit

---
+++ src/MultipleOfTwo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MultipleOfTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number to be multiply by 2: ");
+        int number = scanner.nextInt();
+
+        int count = 1;
+        int two = 2;
+
+        while (count < 50){
+            int isTwo = count * two;
+            System.out.println("Multiplying by: "+ isTwo);
+            count++;
+        }
+
+
+    }
+}
